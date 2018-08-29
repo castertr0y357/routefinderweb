0d065b68cf64ea79b44fc865a964c0bc18ed2e01
Jimmy Fort <castertr0y357@hotmail.com>
2018-08-29 13:28:31 -0400

    updated with print_points and more of the actual route finder algorithm.

--- untitled/RouteFinder.py
+++ untitled/RouteFinder.py
@@ -2,9 +2,6 @@ import googlemaps
 gapi = "AIzaSyC2mfL58CI4oSI31dB9afbJZ5EN_wDQirg"
 gmaps = googlemaps.Client(key=gapi)
 distance = gmaps.distance_matrix
-input_points = [""] * 20
-map_points = []
-map_order = []
 
 
 class Point:
@@ -18,7 +15,7 @@ class Point:
     @staticmethod
     def find_distance(point1, point2):
         result = distance(point1.address, point2.address, units="imperial")
-        point2.value = result["rows"][0]["elements"][0]["duration"]["value"]  #Value in seconds
+        point2.value = result["rows"][0]["elements"][0]["duration"]["value"]  # Value in seconds
         return point2.value
 
     @staticmethod
@@ -43,11 +40,83 @@ class Point:
             map_points.append(Point(x))
         return
 
+    @staticmethod
+    def print_points():
+        counter = 1
+        for x in map_points:
+            if x == "":
+                pass
+            else:
+                print(counter + " : ")
+                print(x.address)
+        return
 
-pointa = "141 daneborg rd durham nc 27703"
+    @staticmethod
+    def create_route():
+        center = (map_order.__len__() / 2) - 1
+        reference = center
+        left = reference - 1
+        right = left + 1
+        left_open = True
+        right_open = True
+
+        Point.create_points(input_points)
+
+        # find furthest point
+
+        for x in map_points:
+            Point.find_distance(home, x)
+        map_order[center] = Point.find_max(map_points)
+
+        # remove furthest point and add home into map points
+
+        map_points.remove(map_order[center])
+        map_points.append(home)
+
+        # find left and right points
+
+        for x in map_points.__len__():
+            if left_open and right_open:
+
+                # find distances from reference
+
+                for y in map_points:
+                    Point.find_distance(reference, y)
+
+                min_point = Point.find_min(map_points)
+
+                # if right point is closer, set right point
+
+                if Point.find_distance(min_point, map_order[right]) < min_point.value:
+                    right += 1
+                    map_order[right] = min_point
+
+                    if min_point == home:
+                        right_open = False
+                else:
+                    map_order[left] = min_point
+                    left -= 1
+
+                    if min_point == home:
+                        left_open = False
+
+            else if left_open:
+                
+
+
+
+
+
+pointa = "12677 newfield drive orlando fl 32837"
 pointb = "3030 slater rd morrisville nc 27560"
 
-input_points[10] = pointb
+home = Point("141 daneborg rd durham nc 27703")
+
+input_points = [pointa,pointb]
+map_points = []
+map_order = [""] * (input_points.__len__() * 2)
+Point.create_route()
+
 """furthest = Point.find_max(arr)
 #closest = Point.find_min(arr)
 #print(furthest.address)
@@ -56,6 +125,4 @@ Point.create_points(input_points)
 print(map_points[1].address)
 Point.find_distance(map_points[0], map_points[1])
 print(map_points[1].value)"""
-
-print(input_points)
 
