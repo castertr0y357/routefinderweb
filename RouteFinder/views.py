9b1d59c91573e33a1f1993707fb5fed1c286bec9
castertr0y357 <castertr0y357@hotmail.com>
2018-10-08 11:10:24 -0400

    Re-organized some of the content, and fixed root url path file.

--- RouteFinder/views.py
+++ RouteFinder/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world.  You are at the Routefinder index.")
+class IndexView(generic.TemplateView):
+    def index(self):
+        return HttpResponse("Hello, world.  You are at the Routefinder index.")
